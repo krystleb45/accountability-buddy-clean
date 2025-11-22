27a7d7dc094040f83bbc7ce9dfb6b19d7833494e
Anaam Khuram <anaam.khuram@gmail.com>
2025-11-22 20:48:54 +0000

    📦chore: next swc patch

--- packages/transactional/types/react-email.d.ts
+++ packages/transactional/types/react-email.d.ts
@@ -1,16 +1,28 @@
-import React from 'react';
+import React from "react"
 
-declare module '@react-email/components' {
-  export const Html: React.ComponentType<React.HTMLAttributes<HTMLHtmlElement>>;
-  export const Head: React.ComponentType<React.HTMLAttributes<HTMLHeadElement>>;
-  export const Body: React.ComponentType<React.HTMLAttributes<HTMLBodyElement>>;
-  export const Preview: React.ComponentType<{ children?: React.ReactNode }>;
-  export const Container: React.ComponentType<React.HTMLAttributes<HTMLTableElement>>;
-  export const Section: React.ComponentType<React.HTMLAttributes<HTMLTableElement>>;
-  export const Img: React.ComponentType<React.ImgHTMLAttributes<HTMLImageElement>>;
-  export const Heading: React.ComponentType<React.HTMLAttributes<HTMLHeadingElement>>;
-  export const Text: React.ComponentType<React.HTMLAttributes<HTMLParagraphElement>>;
-  export const Button: React.ComponentType<React.AnchorHTMLAttributes<HTMLAnchorElement>>;
-  export const render: (component: React.ReactElement) => Promise<string>;
-  export const toPlainText: (html: string) => string;
+declare module "@react-email/components" {
+  export const Html: React.ComponentType<React.HTMLAttributes<HTMLHtmlElement>>
+  export const Head: React.ComponentType<React.HTMLAttributes<HTMLHeadElement>>
+  export const Body: React.ComponentType<React.HTMLAttributes<HTMLBodyElement>>
+  export const Preview: React.ComponentType<{ children?: React.ReactNode }>
+  export const Container: React.ComponentType<
+    React.HTMLAttributes<HTMLTableElement>
+  >
+  export const Section: React.ComponentType<
+    React.HTMLAttributes<HTMLTableElement>
+  >
+  export const Img: React.ComponentType<
+    React.ImgHTMLAttributes<HTMLImageElement>
+  >
+  export const Heading: React.ComponentType<
+    React.HTMLAttributes<HTMLHeadingElement>
+  >
+  export const Text: React.ComponentType<
+    React.HTMLAttributes<HTMLParagraphElement>
+  >
+  export const Button: React.ComponentType<
+    React.AnchorHTMLAttributes<HTMLAnchorElement>
+  >
+  export const render: (component: React.ReactElement) => Promise<string>
+  export const toPlainText: (html: string) => string
 }
