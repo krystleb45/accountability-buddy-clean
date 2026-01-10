2a6f0ad774759afd2b6474ea43bddc5932e9f802
Krsytle Berry <krystle.monet@gmail.com>
2026-01-10 18:59:30 -0500

    pricing update

--- apps/frontend/src/components/pricing.tsx
+++ apps/frontend/src/components/pricing.tsx
@@ -100,9 +100,9 @@ export function Pricing({
         </TabsList>
 
         <div
-          className={cn(`mt-12 grid grid-cols-1 gap-6`, {
-            "md:grid-cols-2 lg:grid-cols-4": showFreeTrial,
-            "md:grid-cols-3": !showFreeTrial,
+          className={cn(`mx-auto mt-12 grid max-w-3xl grid-cols-1 gap-6`, {
+            "md:grid-cols-2": showFreeTrial,
+            "md:grid-cols-1 max-w-md": !showFreeTrial,
           })}
         >
           {pricingPlans.map((plan, index) => (
